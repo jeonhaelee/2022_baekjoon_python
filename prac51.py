52a19b8888580ee7f8d8542203164bfb682e9525
jeonhaelee <98138782+jeonhaelee@users.noreply.github.com>
2022-11-27 23:55:27 +0900

    2839_fin

--- prac51.py
+++ prac51.py
@@ -1,33 +1,14 @@
 # 2839 - 설탕배달
 
-import sys
-
 num = int(input())
-
-# 5로 나누어 떨어질 때
-if num % 5 == 0:
-    print(num // 5)
-    sys.exit()
-    
-# 5로 나누어 떨어지고, 3으로 나누어 떨어질 때
-answer = 0
-while True:
-    answer += num // 5
-    remain = num % 5
-    if remain % 3 != 0:
-        answer = 0
+count = 0
+while num >= 0 :
+    if num % 5 == 0 :
+        count += (num // 5)
+        print(count)
         break
-    answer += remain // 3
-    break
-
-if answer > 0:
-    print(answer)
-    sys.exit()
+    num -= 3  
+    count += 1 
+else :
+    print(-1)
     
-# 3으로 나누어 떨어질 때
-if num % 3 == 0:
-    print(num // 3)
-    sys.exit()
-    
-# 그 외
-print(-1)

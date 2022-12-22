d2970a10327ba5cd9d422745df1d4447f11386f4
jeonhaelee <98138782+jeonhaelee@users.noreply.github.com>
2022-12-22 23:06:04 +0900

    5052_nonfin

--- prac65.py
+++ prac65.py
@@ -3,7 +3,17 @@
 import sys
 input = sys.stdin.readline
 
+def check_number(call_nums):
+    for i in range(len(call_nums)):
+        for j in range(i+1, len(call_nums)):
+            if call_nums[j].startswith(call_nums[i]):
+                return 'NO'
+    return 'YES'
+    
 t = int(input())
 for _ in range(t):
     n =  int(input())
-    call_nums = [int(input()) for _ in range(n)]
+    call_nums = [input()[:-1] for _ in range(n)]
+    call_nums.sort(key=lambda x:len(x))
+    print(check_number(call_nums))
+    

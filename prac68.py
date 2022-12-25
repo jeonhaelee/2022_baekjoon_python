b6ec6a6924123d9c21289f0819df615ad648cd65
jeonhaelee <98138782+jeonhaelee@users.noreply.github.com>
2022-12-26 01:57:44 +0900

    1766_fin

--- prac68.py
+++ prac68.py
@@ -2,3 +2,31 @@
 
 import sys
 input = sys.stdin.readline
+
+import heapq
+
+n, m = map(int, input().split())
+people = [0] * (n+1)
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    people[b] += 1
+    
+que = []
+answer = []
+
+for i in range(1, n+1):
+    if people[i] == 0:
+        heapq.heappush(que, i)
+        
+while que:
+    p = heapq.heappop(que)
+    for k in graph[p]:
+        people[k] -= 1
+        if people[k] == 0:
+            heapq.heappush(que, k)
+    answer.append(p)
+
+print(*answer)

940e8466b54e52d6d9498a9bb69790d09501b76d
jeonhaelee <98138782+jeonhaelee@users.noreply.github.com>
2022-12-29 00:27:35 +0900

    11497_fin

--- prac71.py
+++ prac71.py
@@ -8,9 +8,9 @@ t = int(input())
 for _ in range(t):
     n = int(input())
     arr = list(map(int, input().split()))
-    arr.sort(reverse=True)
+    arr.sort(reverse=True) # 내림차순 정렬
     result = 0
-    for i in range(n-2):
+    for i in range(n-2): # 내림차순 정렬을 해놨기 때문에, i와 i+1 관계는 확인 안 해주어도 됨!
         result = max(result, arr[i] - arr[i+2])
     print(result)
     
